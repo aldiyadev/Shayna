c2b45c8e2c4d185722da2e7ffd7188412da80399
aldiyahya123 <aldiyahya123@gmail.com>
2020-11-08 11:13:52 +0700

    first upload

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <Header />
+    <Hero />
+    <WomanShayna />
+    <Instagram />
+    <Partner />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import WomanShayna from "../components/WomanShayna";
+import Instagram from "../components/Instagram";
+import Partner from "../components/Partner";
+import Footer from "../components/Footer";
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+    Hero,
+    WomanShayna,
+    Instagram,
+    Partner,
+    Footer,
+  },
+};
+</script>
